1a0b44812fb1aab4945b4f5734593277697e92e0
xlyang0211 <xlyang0211@126.com>
2014-12-19 19:26:47 +0800

    transform fraction to decimal:passed

--- fraction_to_recurring_decimal.py
+++ fraction_to_recurring_decimal.py
@@ -3,37 +3,49 @@ from __future__ import division
 remnant_hash = {}
 class Solution:
     # @return a string
-    def get_decimals(self, result, number, remnant, denominator): # here remnant must be smaller than denominator
-        print "remnant is:", remnant
-        if remnant in remnant_hash.keys():
-            i = remnant_hash[remnant]
-            result = result[:i] + "(" + result[i:] + ")"
-            return result
-        else:
-            print "enter here"
-            remnant = 10 * remnant
-            quotient = remnant // denominator
-            remnant = remnant % denominator
-            result = result + str(quotient)
-            remnant_hash[remnant] = number
-            result = self.get_decimals(result, number+1, remnant, denominator)
-            print "The result is:", result
-
     def fractionToDecimal(self, numerator, denominator):
-
+        remnant_hash = {}
+        count = 0
         result = ""
+        result_integar = ""
+        result_decimal = ""
+        sign = ""
+        if numerator < 0 and denominator > 0:
+            sign = "-"
+            numerator = 0 - numerator
+        elif numerator > 0 and denominator < 0:
+            sign = "-"
+            denominator = 0 - denominator
+        elif numerator < 0 and denominator < 0:
+            numerator = 0 - numerator
+            denominator = 0 - denominator
+        else:
+            pass
         quotient = numerator // denominator
         remnant = numerator % denominator
-        result = str(quotient) + "."
-        result = result + self.get_decimals("", 1, remnant, denominator)
+        if remnant == 0:
+            result = sign + str(quotient)
+        else:
+            result_integar = str(quotient) + "."
+            result_decimal = ""
+            while remnant not in remnant_hash.keys():
+                remnant_hash[remnant] = count
+                count += 1
+                quotient = 10 * remnant // denominator
+                remnant = 10 * remnant % denominator
+                result_decimal = result_decimal + str(quotient)
+                if remnant == 0: # which means that there is no recurring decimals
+                    break
+            if remnant == 0:
+                result = sign + result_integar + result_decimal
+            else:
+                num = remnant_hash[remnant]
+                result = sign + result_integar + result_decimal[:num] + "(" + result_decimal[num:] + ")"
         return result
-
-
-
 
 if __name__ == "__main__":
     solution = Solution()
-    result = solution.fractionToDecimal(8, 11)
+    result = solution.fractionToDecimal(7, -12)
     print result
 
 
